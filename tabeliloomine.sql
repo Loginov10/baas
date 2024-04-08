5530d1b63995b45482dc0daa50bd8735eb92d801
Artjom Loginov <artjomloginov111@gmail.com>
2024-04-08 14:53:47 +0300

    insert into on valmis 2

--- tabeliloomine.sql
+++ tabeliloomine.sql
@@ -25,3 +25,35 @@ VALUES
 ('Kiki','Trub','5853201','Tallinn','2023-08-26'),
 ('Lame','Hekko','5878321','Tallinn','2023-08-26');
 SELECT * FROM opilane;
+
+---------------------------------------------
+
+CREATE TABLE language
+(
+ID int NOT NULL PRIMARY KEY,
+Code char NOT NULL,
+Language varchar(50) NOT NULL,
+IsOfficial bit,
+Percentage decimal(5, 2)
+);
+
+--tabeliandmete lisamine
+Select * FROM language;
+INSERT INTO language (ID, Code, Language, IsOfficial, Percentage)
+VALUES (100, 'EST','Eesti', 1, 80.5);
+
+--tabeli struktuuri muutmine
+ALTER TABLE Language ADD Capital varchar(20);
+
+--veergu kustutamine
+ALTER TABLE Language DROP COLUMN Capital;
+
+INSERT INTO language (ID, Code, Language, IsOfficial, Percentage, Capital)
+VALUES (101, 'FIN','Soome', 1, 96.5,'Helsingi');
+
+--uuendamine
+UPDATE language SET Capital= 'Tallinn'
+WHERE ID=100;
+
+--tabeli kustutamine
+DROP TABLE language;
