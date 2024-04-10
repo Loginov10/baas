8a01ab1e6d56b80cf43f61214aa1a76b20b214be
Artjom Loginov <atjomloginov111@gmail.com>
2024-04-10 14:14:39 +0300

    tundide töö

--- tabeliloomine.sql
+++ tabeliloomine.sql
@@ -14,7 +14,7 @@ aadress TEXT,
 sisseastumis_kp date
 );
 --primary key - primaarne võti mis annab unikaalsus
---identity(1,1) - määrab: ingaühele oma numbri
+--identity(1,1) - määrab: ingaühele oma numbri --väli täidetakse automaatselt
 
 SELECT * FROM opilane;
 
@@ -57,3 +57,81 @@ WHERE ID=100;
 
 --tabeli kustutamine
 DROP TABLE language;
+-----------
+
+--tabeli rühm loomine
+CREATE TABLE ryhm(
+ryhmId int Primary key identity (1,1),
+ryhmNimetus varchar(20) UNIQUE,
+osakond char(3));
+
+Select * from ryhm;
+SELECT * FROM opilane;
+
+INSERT INTO ryhm (ryhmNimetus,osakond,juhatajaID)
+VALUES ('TARpv23','IT',2);
+
+
+--FOREIGN KEY --> PK teises tabelis
+ALTER TABLE opilane ADD ryhmID int;
+
+--tabeli opilane uuendamine
+Update opilane SET ryhmId=2;
+
+--Fk lisamine opilane tabelisse
+ALTER TABLE opilane
+ADD FOREIGN KEY (ryhmID) REFERENCES ryhm(ryhmID);
+
+INSERT INTO opilane(
+eesnimi, perenimi, isikukood, aadress, sisseastumis_kp,ryhmId)
+VALUES
+('Test','Test','5855301','Tallinn','2023-08-26',1);
+SELECT * FROM opilane;
+
+--Drop table nazvanie, Alter table.... drop column....
+--telefon varchar(15)
+--tabeli ryhmajuhataja loomine
+CREATE TABLE ryhmajuhataja(
+juhatajaId int Primary key identity (1,1),
+eesnimi varchar(20),
+perenimi varchar(20),
+telefon varchar(20),
+);
+SELECT * FROM ryhmajuhataja;
+
+INSERT INTO ryhmajuhataja(eesnimi, perenimi, telefon)
+VALUES ('Art','Log','621092');
+SELECT * FROM ryhmajuhataja;
+select * from ryhm
+
+--veergu lisamine tabelisse
+ALTER TABLE ryhm add juhatajaID int;
+--tabeli ryhm uuendamine
+Update ryhm SET juhatajaID=1;
+
+ALTER TABLE ryhm
+ADD FOREIGN KEY (juhatajaID) REFERENCES ryhmajuhataja(juhatajaID);
+
+INSERT INTO ryhm (ryhmNimetus,osakond,juhatajaID)
+VALUES ('TARpv22','IT',1);
+
+--tabeli hinnad loomine
+CREATE TABLE hinnad(
+hinnangID int Primary key identity (1,1),
+kuupaeb date,
+id int,
+juhatajaID int,
+hinnang text
+);
+
+SELECT * FROM hinnad
+
+INSERT INTO hinnad(kuupaeb,id,juhatajaID,hinnang)
+VALUES('2023-08-26',1,1,'4');
+
+ALTER TABLE hinnad
+ADD FOREIGN KEY (id) REFERENCES opilane(id);
+
+ALTER TABLE hinnad
+ADD FOREIGN KEY (juhatajaid) REFERENCES ryhmajuhataja(juhatajaid);
+
