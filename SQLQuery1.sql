45985df54f04dbb21fb0a16f84c0b07bbaa4363a
Artjom Loginov <artjomloginov111@gmail.com>
2024-05-08 13:00:59 +0300

    loome stored procedure,

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -286,12 +286,12 @@ spGetEmployees
 exec spGetEmployees
 execute spGetEmployees
 
---- 
+--- loome stored procedure
 create proc spGetEmployeesByGenderAndDepartment
 @Gender nvarchar(20),
 @DepartmentId int
 as begin
-	select FirstName, Gender, DepartmentId from Employees where Gender = @Gender
+	select Name, Gender, DepartmentId from Employees where Gender = @Gender
 	and DepartmentId = @DepartmentId
 end
 
