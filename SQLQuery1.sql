965aac83a75ff8a429788927b964b7e716d65a55
Artjom Loginov <artjomloginov111@gmail.com>
2024-05-08 12:19:50 +0300

    vaatame tabeli andmeid

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -57,7 +57,7 @@ values (7, 'Spiderman', 'spider@spiderman.com', 2)
 -- vaatame tabeli andmeid
 select * from Person
 
---- võõrvõtme piirangu maha võtmine
+--- võõrvõtme piirangu maha võtmine.
 alter table Person
 drop constraint tblPerson_GenderId_FK
 
